b45e2221d477fdf2a850c28b5f71dfe00d2ce34e
MrSchlag <oscar.lambrun@club-internet.fr>
2021-10-02 18:24:37 +0200

    capsule for people and no sofness on join

--- scenes/world/people/people.gd
+++ scenes/world/people/people.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta: float) -> void:
+	#if ()
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
 	elif Input.is_action_pressed("ui_right"):
@@ -16,7 +17,8 @@ func _process(delta: float) -> void:
 		direction = 0
 
 func _physics_process(delta: float) -> void:
-	#var side_factor = 0 - position.x
+	var distance_side_to_player: int
 	
 	
+		
 	apply_central_impulse(Vector2(direction * speed, 0))
