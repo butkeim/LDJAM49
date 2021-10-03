e4363170f9df1515a2cb7d72f87cee080ca48acf
MrSchlag <oscar.lambrun@club-internet.fr>
2021-10-03 18:33:15 +0200

    code fix

--- scenes/world/bird/bird_spawner.gd
+++ scenes/world/bird/bird_spawner.gd
@@ -10,11 +10,9 @@ export var threshold_min = 1
 export var threshold_max = 4
 export var spawn_distance = 700
 
-
 func _ready() -> void:
 	randomize()
 	cabin = $"../Cabin/RigidBody2D/"
-
 
 func _process(delta: float) -> void:
 	timer += delta
@@ -22,15 +20,16 @@ func _process(delta: float) -> void:
 	if timer > threshold:
 		timer = 0
 		threshold = rand_range(threshold_min, threshold_max)
-		var rotate
-		if rand_range(0, 1) > 0.5:
-			rotate = rand_range(-50, 50)
-		else:
-			rotate = rand_range(130, 230)
-		print(rotate)
+		
 		var spawn_vec = Vector2(cabin.position.x + spawn_distance, cabin.position.y)
-		var spawn_vec_rotated = spawn_vec.rotated(deg2rad(rotate))
+		var spawn_vec_rotated = spawn_vec.rotated(deg2rad(get_random_valid_angle()))
 		var instance_bird = bird.instance()
 		instance_bird.position = spawn_vec_rotated
 		instance_bird.speed = 100
 		get_parent().add_child(instance_bird)
+
+func get_random_valid_angle() -> float:
+	if rand_range(0, 1) > 0.5:
+		return rand_range(-50, 50)
+	else:
+		return rand_range(130, 230)
