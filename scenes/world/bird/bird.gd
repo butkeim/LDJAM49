6a15c2b0c68aedeb39cd3e5a743228c2971f6bdf
MrSchlag <oscar.lambrun@club-internet.fr>
2021-10-03 15:43:05 +0200

    use area2d to move cabin

--- scenes/world/bird/bird.gd
+++ scenes/world/bird/bird.gd
@@ -2,7 +2,11 @@ extends RigidBody2D
 
 export (float) var speed
 
-var cabin: RigidBody2D
+signal entered_cabin(force, position)
+
+var cabin_rigidbody: RigidBody2D
+var cabin: Node2D
+var area: Area2D
 var direction: Vector2
 var has_attaked = false
 var has_touched = false
@@ -11,24 +15,27 @@ var initial_position: Vector2
 
 func _ready() -> void:
 	print("spawn bird")
-	initial_position = position
+	initial_position = Vector2(global_position.x + rand_range(-400, 400), global_position.y + rand_range(-400, 400))
 	contact_monitor = true
 	contacts_reported = 2
-	connect("body_entered", self, "body_entered_handler")
-	cabin = $"../Cabin/RigidBody2D/"
+	area = $"Area2D"
+	area.connect("body_entered", self, "body_entered_handler")
+	cabin_rigidbody = $"../Cabin/RigidBody2D/"
+	cabin = $"../Cabin/"
+	connect("entered_cabin", cabin, "apply_force_at_handler")
 	direction = position.direction_to(cabin.position)
 	
 	set_deferred("linear_velocity", Vector2(direction.x * speed, direction.y * speed))
-	#set_applied_force(Vector2(direction.x * speed, direction.y * speed))
+	set_applied_force(Vector2(direction.x * speed, direction.y * speed))
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if is_near_cabin() && !has_attaked:
 		has_attaked = true
 		add_central_force(Vector2(direction.x * speed * 20, direction.y * speed * 20))
 	if has_touched && !is_on_return:
 		is_on_return = true
 		var return_direction = position.direction_to(initial_position)
-		add_central_force(Vector2(return_direction.x * speed, return_direction.y * speed))
+		add_central_force(Vector2(return_direction.x * speed * 100, return_direction.y * speed * 100))
 
 func is_near_cabin() -> bool:
 	var distance_to_cabin = position.distance_to(cabin.position)
@@ -36,7 +43,9 @@ func is_near_cabin() -> bool:
 
 func body_entered_handler(body: Node):
 	if body.is_in_group("cabin"):
-		$CollisionShape2D.set_deferred("disabled", true)
+		emit_signal("entered_cabin", linear_velocity, position)
+		$"Area2D".set_deferred("monitoring", false)
+		print("body entered");
 		has_touched = true
 		
 func _on_VisibilityNotifier2D_screen_exited() -> void:
--- scenes/world/cabin/cabin.gd
+++ scenes/world/cabin/cabin.gd
@@ -4,14 +4,25 @@ export (float) var death_angle
 var cabin_rigid_body: RigidBody2D
 var join: PinJoint2D
 var hasFallen = false
+var next_force_to_apply: Vector2
+var next_force_position_to_apply: Vector2
+var apply_force = false
 
 func _ready() -> void:
 	cabin_rigid_body = $RigidBody2D
 	join = $PinJoint2D
-	
 
 func _physics_process(delta: float) -> void:
-	print(delta)
+	if apply_force:
+		apply_force = false
+		cabin_rigid_body.apply_impulse(next_force_position_to_apply, next_force_to_apply)
 	if !hasFallen && (cabin_rigid_body.rotation_degrees > death_angle || cabin_rigid_body.rotation_degrees < -death_angle):
 		hasFallen = true
 		remove_child(join)
+
+func apply_force_at_handler(force: Vector2, position: Vector2):
+	print("received")
+	next_force_position_to_apply = position
+	next_force_to_apply = force
+	apply_force = true
+	
