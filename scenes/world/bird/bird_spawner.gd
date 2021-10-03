fc0f114e51de2ebb52f2ba30de8a5bd329090377
MrSchlag <oscar.lambrun@club-internet.fr>
2021-10-03 18:15:29 +0200

    Update bird_spawner.gd

--- scenes/world/bird/bird_spawner.gd
+++ scenes/world/bird/bird_spawner.gd
@@ -22,8 +22,14 @@ func _process(delta: float) -> void:
 	if timer > threshold:
 		timer = 0
 		threshold = rand_range(threshold_min, threshold_max)
+		var rotate
+		if rand_range(0, 1) > 0.5:
+			rotate = rand_range(-50, 50)
+		else:
+			rotate = rand_range(130, 230)
+		print(rotate)
 		var spawn_vec = Vector2(cabin.position.x + spawn_distance, cabin.position.y)
-		var spawn_vec_rotated = spawn_vec.rotated(deg2rad(rand_range(0, 360)))
+		var spawn_vec_rotated = spawn_vec.rotated(deg2rad(rotate))
 		var instance_bird = bird.instance()
 		instance_bird.position = spawn_vec_rotated
 		instance_bird.speed = 100
